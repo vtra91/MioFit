2c2da95ab1f204613cfe904ed7e7cdb22ad9bc6a
vtra9 <melnikov0109@gmail.com>
2026-08-18 14:38:36 +0800

    Initial Commit

---
+++ MioFit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MioFit
+//
+//  Created by Сергей Мельников on 18.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MioFit/MioFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MioFitApp.swift
+//  MioFit
+//
+//  Created by Сергей Мельников on 18.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct MioFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
