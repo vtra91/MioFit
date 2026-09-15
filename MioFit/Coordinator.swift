c197ba05d7a647c361792c098ce7db1ec562077c
vtra9 <melnikov0109@gmail.com>
2026-09-15 11:52:12 +0800

    feature: первично реализовал паттерн AppCoordinator, пока что только для экранов Auth и AllClients

--- MioFit/Coordinator.swift
+++ MioFit/Coordinator.swift
@@ -1,2 +1,50 @@
+import Combine
+import SwiftUI
 
-// https://habr.com/ru/articles/892758/
+enum MioScreens: Hashable {
+    case login
+    case home
+}
+
+@MainActor
+final class MioState: ObservableObject {
+    @Published var isAuth = false
+    func login() {
+        isAuth = true
+    }
+    func logout() {
+        isAuth = false
+    }
+}
+
+@MainActor
+final class MioRouter: ObservableObject {
+    @Published var path = NavigationPath()
+    
+    func push(_ screen: MioScreens) {
+        path.append(screen)
+    }
+    func pop() {
+        if !path.isEmpty {
+            path.removeLast()
+        }
+    }
+    func popToRoot() {
+        path = NavigationPath()
+    }
+}
+
+@MainActor
+final class MioCoordinator: ObservableObject {
+    private let router: MioRouter
+    private let currState: MioState
+    init(router: MioRouter, currState: MioState) {
+        self.router = router
+        self.currState = currState
+    }
+    func successLogin() {
+        currState.login( )
+    }
+}
+
+
--- MioFit/MioFitApp.swift
+++ MioFit/MioFitApp.swift
@@ -3,10 +3,51 @@ import SwiftUI
 @main
 struct MioFitApp: App {
     @AppStorage("isDarkMode") private var isDarkMode = false
+    @StateObject private var router: MioRouter
+    @StateObject private var coordinator: MioCoordinator
+
+    @StateObject var currState = MioState()
+
     var body: some Scene {
+
+//TODO: починить корявое отображение анимации (transition не работает)
+        
         WindowGroup {
-            Auth()
-                .preferredColorScheme(isDarkMode ? .dark : .light)
+            Group {
+                    if currState.isAuth {
+                        NavigationStack(path: $router.path.animation(.linear(duration: 0))) {
+                            ClientsListView()
+                                .transition(.asymmetric(
+                                    insertion: .move(edge: .trailing),
+                                    removal: .move(edge: .leading)
+                                ))
+                                .navigationDestination(for: MioScreens.self) { currScreen in
+                                    switch currScreen {
+                                    case MioScreens.home: ClientsListView()
+                                    case MioScreens.login: Auth()
+                                    }
+                                }
+                        }
+                    } else {
+                        Auth()
+                            .transition(.asymmetric(
+                                insertion: .move(edge: .trailing),
+                                removal: .move(edge: .leading)
+                            ))
+                    }
+            }
+            
+            .preferredColorScheme(isDarkMode ? .dark : .light)
+            .environmentObject(coordinator)
         }
     }
+    init() {
+            let tempRouter = MioRouter()
+            let tempState = MioState()
+            let tempCoordinator = MioCoordinator(router: tempRouter, currState: tempState)
+            
+            self._router = StateObject(wrappedValue: tempRouter)
+            self._currState = StateObject(wrappedValue: tempState)
+            self._coordinator = StateObject(wrappedValue: tempCoordinator)
+        }
 }
